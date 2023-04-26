8d2f13d3ed168f39e2bbb374d8b1fa6a74c66288
Zalvorix <114089240+Zalvorix@users.noreply.github.com>
2023-04-26 08:20:17 -0500

    Initial commit

---
+++ Classes_Olivia_Babcock/Greetings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Olivia_Babcock
+{
+    class Greetings
+    {  
+        /// <summary>
+        /// Says hello to the user
+        /// </summary>
+        public void Welcome()
+        {
+            Console.WriteLine("Hello Traveler, welcome!");
+        }
+        /// <summary>
+        /// Says the user's presence is welcome
+        /// </summary>
+        /// <param name="name"></param>
+        public void Hello(string name)
+        {
+            Console.WriteLine($"Your presence is a welcome addition, {name}.");
+        }
+            
+    }
+}
---
+++ Classes_Olivia_Babcock/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Classes_Olivia_Babcock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // Creates a new greeting
+            Greetings greetings = new Greetings();
+           // Prints "Hello Traveler, welcome!" to the console
+            greetings.Welcome();
+
+            // Asks user for their name
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            
+            // Defines the user's name
+            greetings.Hello(userName);
+        }
+    }
+}
